bf9f71f9fa843d2fd9250692ba176fa335e2335d
Mateusz Kaczmarczyk <math.k999@gmail.com>
2021-05-03 15:31:00 +0200

    Init blender plugin for tic tac toe terrain

---
+++ app.py
@@ -0,0 +1,13 @@
+bl_info = {
+    "name": "Tic Tac Toe Terrain",
+    "blender": (2, 80, 0),
+    "category": "Object",
+}
+
+
+def register():
+    print("Hello Tic Tac Toe Terrain")
+
+
+def unregister():
+    print("Goodbye Tic Tac Toe Terrain")
